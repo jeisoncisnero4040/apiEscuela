51fa3da76fc65f58776368ed43b31e99547036c3
jeisoncisnero4040 <legoladhojaverde@gmail.com>
2024-06-10 10:43:54 -0300

    solucion bug de login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <style>
+        .container {
+            background-color: black;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;  
+            text-align: center;  
+            flex-direction: column;
+        }
+        p {
+            display: block;
+            font-size: 18px;  
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;  
+            color: white;
+            margin: 5px;  
+        }
+        small {
+            font-size: 14px;  
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;  
+            color: white;
+        }
+        h2{
+            font-size: 20px;  
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;  
+            color: white;
+            margin: 5px;  
+        }
+    </style>
+</head>
+<body>
+    <div class='container'>
+        <p>Hidalgo School API Developers</p>
+        <p>Authors:</p>
+        <p>DatamatrixCompany</p>
+        <h2>Contact Us</h2>
+        <p>If you have any questions or need assistance, please feel free to <a href="https://datamatrixcompany.com/">contact us</a>.</p>
+        <small>All rights reserved. &copy; 2024 Hidalgo School</small>
+    </div>
+</body>
+</html>
